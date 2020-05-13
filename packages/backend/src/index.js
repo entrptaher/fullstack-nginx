58d02fc077e3824cc3710673038561e6a6cb5e48
Ashiqur Rahman <ashiquejava@gmail.com>
2020-05-13 15:44:34 +0600

    fix: get correct ip from localhost with reverse proxy

---
+++ packages/backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import createServer from "./loaders/server";
+
+const server = createServer();
+
+server.start(
+  {
+    port: 4001,
+    cors: {
+      credentials: true,
+      origin: "http://localhost:1234",
+    },
+    debug: true,
+  },
+  (listener) => {
+    console.log(`Server is listening on port ${listener.port}`);
+  }
+);
